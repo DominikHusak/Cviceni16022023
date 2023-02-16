28975e71fcb7226c299d43b7203c84ebe00960ec
Dominik Husák <husak2@SPSEJECNANET.CZ>
2023-02-16 08:58:56 +0100

    third commit

--- script.js
+++ script.js
@@ -1,10 +1,55 @@
 class Zamestnanci{
-    constructor(name, position, salary, start_date, office, extn){
+    constructor(name, text, price, mark, category){
         this.name = name;
-        this.position = position;
-        this.salary = salary;
-        this.start_date = start_date;
-        this.office = office;
-        this.extn = extn;
+        this.text = text;
+        this.price = price;
+        this.mark = mark;
+        this.category = category;
     }
+}
+
+let zamestanci = [];
+
+function reqListener() {
+    console.log(this.responseText);
+}
+  
+  const req = new XMLHttpRequest();
+  req.addEventListener("load", reqListener);
+  req.open("GET", "https://datausa.io/api/data?drilldowns=Nation&measures=Population");
+  req.send();
+
+  function save(i) {
+      localStorage.setItem('data', JSON.stringify(zamestnanci));
+  }
+
+  function load(i) {
+      zamestanci = [];
+      zamestanci = JSON.parse(localStorage.getItem('data'))
+  }
+
+req.addEventListener("progress", updateProgress);
+req.addEventListener("load", transferComplete);
+req.addEventListener("error", transferFailed);
+req.addEventListener("abort", transferCanceled);
+
+req.open();
+
+function updateProgress(event) {
+  if (event.lengthComputable) {
+    const percentComplete = (event.loaded / event.total) * 100;
+  } else {
+  }
+}
+
+function transferComplete(evt) {
+  console.log("The transfer is complete.");
+}
+
+function transferFailed(evt) {
+  console.log("An error occurred while transferring the file.");
+}
+
+function transferCanceled(evt) {
+  console.log("The transfer has been canceled by the user.");
 }
